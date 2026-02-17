d59351f4a17609d1fc835cb93f647409f4456777
Clarissa Ch'ng <clarissachnggg@gmail.com>
2026-02-17 19:47:40 +0000

    feat: working visualised and smoother graph

--- arduino/arduino_magnetometer_example/arduino_magnetometer_example.ino
+++ arduino/arduino_magnetometer_example/arduino_magnetometer_example.ino
@@ -2,26 +2,29 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
-/* Assign a unique ID to this sensor at the same time */
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+Adafruit_HMC5883_Unified mag1 = Adafruit_HMC5883_Unified(12345);
 
 void setup(void) {
   Serial.begin(115200);
-  if(!mag.begin()) {
-    /* There was a problem detecting the HMC5883L ... check your connections */
-    while(1);
+  if(!mag1.begin()) {
+    while(1); // Sensor not found
   }
 }
 
 void loop(void) {
-  sensors_event_t event; 
-  mag.getEvent(&event);
+  sensors_event_t event1;
+  mag1.getEvent(&event1);
 
-  // Send data as a JSON string for easy web parsing
-  Serial.print("{\"x\":"); Serial.print(event.magnetic.x);
-  Serial.print(", \"y\":"); Serial.print(event.magnetic.y);
-  Serial.print(", \"z\":"); Serial.print(event.magnetic.z);
+  Serial.print("{\"x\":"); Serial.print(event1.magnetic.x);
+  Serial.print(",\"y\":"); Serial.print(event1.magnetic.y);
+  Serial.print(",\"z\":"); Serial.print(event1.magnetic.z);
+  
+  /* If you have a second magnet/sensor for squeezing, 
+     calculate x2/y2 and add them to the JSON:
+     Serial.print(",\"x2\":"); Serial.print(val2X);
+     Serial.print(",\"y2\":"); Serial.print(val2Y);
+  */
+  
   Serial.println("}");
-
-  delay(50); // ~20Hz update rate
+  delay(40); // 25Hz output
 }
